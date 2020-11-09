337d8e439d8a866ad9eafda5396159bfe0c68c46
Jivitesh Dalavi <jiviteshdalavi@gmail.com>
2020-11-10 04:50:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt install hostapd
 sudo apt install dnsmasq
+sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
