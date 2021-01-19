3afccc6497a8c335f13f7a6a973a62c003571dcf
Jivitesh Dalavi <jiviteshdalavi@gmail.com>
2021-01-19 16:50:18 +0530

    Update install.sh
    Samba installation

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ sudo apt-get upgrade
 sudo apt install hostapd
 sudo apt install dnsmasq
 sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
+sudo apt-get install samba samba-common-bin
 bash configure.sh
